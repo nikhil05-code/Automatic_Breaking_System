9baffa57c6c3a2a922f782dddbfe5fab522ff188
nikhil05-code <53990224+nikhil05-code@users.noreply.github.com>
2021-11-24 21:26:41 +0530

    Update TIMER.h

--- TIMER.h
+++ TIMER.h
@@ -1,24 +1,27 @@
 #include<lpc214x.h>
 void timer0delay(unsigned int a);
 void timer1delay(unsigned int b);
-void timer0delay(unsigned int a)    //1ms
+
+void timer0delay(unsigned int a)
 {
 		T0TCR=0X02;
-    T0CTCR=0X0000;
-    T0PR=59999;
-    T0MR0=a;
-    T0MCR=0x00000004;
+    T0CTCR=0X00;
+    T0PR=0X1D;
+    T0MR0=a;              
+    T0MCR=0X0004;
     T0TCR=0X01;
     while(T0TC!=T0MR0);
     T0TC=0;
 }
-void timer1delay(unsigned int b)   //1us
+
+
+void timer1delay(unsigned int b)
 {
 		T1TCR=0X02;
-    T1CTCR=0X0000;
-    T1PR=59;
-    T1MR0=b;
-    T1MCR=0x00000004;
+    T1CTCR=0X00;
+    T1PR=0X1D;
+    T1MR0=b;              
+    T1MCR=0X0004;
     T1TCR=0X01;
     while(T1TC!=T1MR0);
     T1TC=0;
